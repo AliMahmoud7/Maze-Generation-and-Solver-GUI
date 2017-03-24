05328a04a10fe1f46f0e2305a30411bd74b8744d
Ali Mahmoud <ce.alimahmoud@gmail.com>
2017-03-24 03:51:42 +0200

    add intial solving

--- generate.py
+++ generate.py
@@ -1,5 +1,5 @@
-# Ali Mahmoud
 # 2017
+# Ali Mahmoud
 
 from tkinter import *
 from random import randrange
@@ -12,16 +12,18 @@ rows, cols = int(height/w), int(width/w)
 grid = [[[] for c in range(cols)] for r in range(rows)]  #using list comprehensions to declare 2d array
 current = 0
 stack = []
+
 solve = []
+backtrack = []
 #
 
 window = Tk()
 window.title('Maze Game!')
-canvas = Canvas(window, width=width, height=height)
-canvas.pack(fill='both')
+canvas = Canvas(window, width=width, height=height, bg='white')
+canvas.pack()
 
 
-class Cell():
+class Cell:
     def __init__(self, i, j):
         self.i = i
         self.j = j
@@ -29,40 +31,45 @@ class Cell():
         self.visited = False
         self.tl = self.rl = self.bl = self.ll = 0
 
+        self.choose = False
+
+        self.rect = 0
+
     def show(self):
         x = self.i * w
         y = self.j * w
         #if self.walls[0]:
-        self.tl = canvas.create_line(x,    y,    x+w,  y, fill='blue')     #draw top line
+        self.tl = canvas.create_line(x,    y,    x+w,  y, fill='blue', width=2)     #draw top line
         #if self.walls[1]:
-        self.rl = canvas.create_line(x+w, y,    x+w , y+w, fill='blue')  #draw right line
+        self.rl = canvas.create_line(x+w, y,    x+w , y+w, fill='blue', width=2)  #draw right line
         #if self.walls[2]:
-        self.bl = canvas.create_line(x+w, y+w, x,     y+w, fill='blue')  #draw bottom line
+        self.bl = canvas.create_line(x+w, y+w, x,     y+w, fill='blue', width=2)  #draw bottom line
         #if self.walls[3]:
-        self.ll = canvas.create_line(x,    y+w, x,     y, fill='blue')     #draw left line
+        self.ll = canvas.create_line(x,    y+w, x,     y, fill='blue', width=2)     #draw left line
 
     def draw_visited(self):
         x = self.i * w
         y = self.j * w
         if self.visited:
-            canvas.create_rectangle(x+2, y+2, x+w-2, y+w-2, fill='white', outline='')
+            self.rect = canvas.create_rectangle(x+9, y+9, x+w-9, y+w-9, fill='white', outline='')
             window.update()
-            time.sleep(0.05)
+            #time.sleep(0.05)
 
-    def hightlight(self):
+    def highlight(self):
         x = self.i * w
         y = self.j * w
-        canvas.create_rectangle(x+2, y+2, x+w-2, y+w-2, fill='red', outline='')
+        canvas.create_rectangle(x+9, y+9, x+w-9, y+w-9, fill='#f7df2e', outline='', tags='rect')
         window.update()
-        time.sleep(0.05)
+        #time.sleep(0.05)
 
-    def check_neighbors(self):
+    def check_neighbors(self, s=False):
         neighbors = []
 
         if not edge(self.i-1, self.j):
             top = grid[self.i-1][self.j]
             if not top.visited:
                 neighbors.append(top)
+
         if not edge(self.i, self.j+1):
             right = grid[self.i][self.j+1]
             if not right.visited:
@@ -106,29 +113,28 @@ def draw():
         for j in range(len(grid[0])):
             grid[i][j].show()
 
-
     while True:
-        current.hightlight()
+        current.highlight()
         current.draw_visited()
 
-        #STEP 1
+        # STEP 1
         next_cell = current.check_neighbors()
         if next_cell:
 
-            #STEP 2
+            # STEP 2
             stack.append(current)
+            solve.append(current)
 
-            #STEP 3
-            if not next_cell.visited:
-                remove_walls(current, next_cell) #######
-                solve.append(current)
+            # STEP 3
+            remove_walls(current, next_cell) #######
 
-            #STEP 4
+            # STEP 4
             current = next_cell
             current.visited = True
 
         elif len(stack) > 0:
             current = stack.pop()
+            backtrack.append(current)
         else:
             break
 
@@ -140,29 +146,76 @@ def remove_walls(c, n):
         if y == -1:
             canvas.delete(c.rl)
             canvas.delete(n.ll)
+            c.walls[1] = False
+            n.walls[3] = False
             window.update()
         elif y == 1:
             canvas.delete(c.ll)
             canvas.delete(n.rl)
+            c.walls[3] = False
+            n.walls[1] = False
             window.update()
 
     if y == 0:
         if x == -1:
             canvas.delete(c.bl)
             canvas.delete(n.tl)
+            c.walls[2] = False
+            n.walls[0] = False
             window.update()
         elif x == 1:
             canvas.delete(c.tl)
             canvas.delete(n.bl)
+            c.walls[0] = False
+            n.walls[2] = False
             window.update()
             #print(c.__dict__)
 
 def draw_solve():
-    global solve
-    solve = grid[0][0]
+    global current
+    solve.reverse()
+    backtrack.reverse()
+    current = solve.pop()
+
+    while True:
+        i = current.i
+        j = current.j
+
+        if i == rows-1 and j == cols-1:
+            current.highlight()
+            break
+
+        if not current.choose:
+            current.highlight()
+            current.choose = True
+            current = solve.pop()
+            window.update()
+            time.sleep(0.1)
+
+        elif current.choose:
+            current.draw_visited()
+            current = backtrack.pop()
+            window.update()
+            time.sleep(0.1)
 
 if __name__ == '__main__':
     setup()
     draw()
     button = Button(window, text='Show Solve!', command=draw_solve).pack()
+
+    def change(event):
+        print('Yes!!')
+        a = canvas.itemconfigure(current.rect, fill='green')
+
+        canvas.update()
+        window.update()
+        b = canvas.itemcget(current.rect, "fill")
+        print('B', b)
+    d = canvas.bind('<Button-1>', change)
+    print('I am bind d = ', d)
+    Label(window, text='Made by: Ali Mahmoud', fg='red', bg='white').pack(side=RIGHT)
+    print(len(solve))
+    print(solve)
+    print(len(backtrack))
+
     window.mainloop()
